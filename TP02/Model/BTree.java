b6c9b9d0b8eee28b95888cb6880fe17ab5295d52
Rafael <rafaelolpeixoto@gmail.com>
2024-04-09 18:41:03 -0300

    Insertion Btree

--- TP02/Model/BTree.java
+++ TP02/Model/BTree.java
@@ -5,7 +5,7 @@ public class BTree {
     public Node root; // Pointer to root node
     public int degree; // Minimum degree
 
-    BTree(int degree) {
+    public BTree(int degree) {
         this.root = null;
         this.degree = degree;
     }
@@ -15,6 +15,8 @@ public class BTree {
 
         if(this.root != null){
             this.root.traverse();
+        }else{
+            System.out.println("Arvore vazia!");
         }
         System.out.println();
     }
@@ -22,15 +24,35 @@ public class BTree {
     // Function to search given key in tree
     public Node search(int key){
         if(this.root == null){
+            System.out.println("Arvore vazia!");
             return null;
         }else{
             return this.root.search(key);
         }
     }
 
+    // Function to insert a new key in the tree
+    public void insert(int key, int address){
+        if (root == null){
+            root = new Node(degree, true);
+            root.keys[0] = key;
+            root.addresses[0] = address;
+            root.keycount = 1; 
+        }else{
+            if (root.keycount == 2 * degree - 1){// If the root is full, then tree grows in height
+                Node newNode = new Node(degree, false);
+                newNode.child[0] = root;
+                newNode.splitChild(0, root);
+                int i = 0;
+                if (newNode.keys[0] < key){
+                    i++;
+                }
+                newNode.child[i].insertNotFull(key, address);
+                root = newNode;
+            }else{
+                root.insertNotFull(key, address);// If root is not full, call insertNotFull for root
+            }
+        }
+    }
 
 }
-
-
-
-
--- TP02/Model/Node.java
+++ TP02/Model/Node.java
@@ -1,19 +1,73 @@
 package Model;
 
 public class Node {
-    int[] key; // An array of keys
-    int degree; //degree (the number of children a node can have)
+    int[] keys; // An array of keys
+    int[] addresses; // An array of addresses
+    int degree; // minimum degree (defines the range for number of keys)
     Node[] child; // An array of child pointers
     int keycount; // Current number of keys
-    boolean leaf; // Is true when node is leaf. Otherwise false
+    boolean isLeaf; // Is true when node is isLeaf. Otherwise false
  
     // Constructor
-    Node(int degree, boolean leaf)
+    Node(int degree, boolean isLeaf)
     {
-        this.leaf = leaf;
-        this.key = new int[degree * 2 - 2];
-        this.child = new Node[degree];
+        this.degree = degree;
+        this.keys = new int[degree * 2 - 1];
+        this.addresses = new int[degree * 2 - 1];
         this.keycount = 0;
+        this.child = new Node[degree];
+        this.isLeaf = isLeaf;
+    }
+
+    void insertNotFull(int key, int address){
+        int i = keycount - 1;
+        if (isLeaf){
+            while (i >= 0 && keys[i] > key){
+                keys[i + 1] = keys[i];
+                addresses[i + 1] = addresses[i];
+                i--;
+            }
+            keys[i + 1] = key;
+            addresses[i + 1] = address;
+            keycount = keycount + 1;
+        }
+    }
+
+    // A utility function to split the child y of this node
+    // Note that y must be full when this function is called
+    void splitChild(int i, Node fullNode){
+        Node newNode = new Node(fullNode.degree, fullNode.isLeaf);
+        newNode.keycount = degree - 1;
+
+        for (int j = 0; j < degree - 1; j++){ // Copy the last (degree - 1) keys of fullNode to newNode
+            newNode.keys[j] = fullNode.keys[j + degree];
+            newNode.addresses[j] = fullNode.addresses[j + degree];
+        }
+
+        if (!fullNode.isLeaf){ // If fullNode is not a leaf, then move all its child pointers and addresses
+            for (int j = 0; j < degree; j++){
+                newNode.child[j] = fullNode.child[j + degree];
+                newNode.addresses[j] = fullNode.addresses[j + degree];
+            }
+        }
+
+        fullNode.keycount = degree - 1; 
+
+        for (int j = keycount; j >= i + 1; j--){// Move all child pointers one space ahead
+            child[j + 1] = child[j];
+        }
+
+        child[i + 1] = newNode;// Link the new child to this node
+
+        for (int j = keycount; j > i; j--){// Move all keys one space ahead
+            keys[j+1] = keys[j];
+            addresses[j+1] = addresses[j];
+        }
+
+        keys[i] = fullNode.keys[degree - 1]; // Copy the middle key of fullNode to this node
+        addresses[i] = fullNode.addresses[degree - 1];
+        keycount++;
+
     }
 
     // Function to traverse all nodes in a subtree rooted with this node
@@ -21,13 +75,13 @@ public class Node {
 
         int i = 0;
         for (i = 0; i < this.keycount; i++) {
-            if (this.leaf == false) {
+            if (!this.isLeaf ) {
                 this.child[i].traverse();
             }
-            System.out.print(this.key[i*2] + this.key[i*2+1] + " ");
+            System.out.print(" " + this.keys[i] + " " + this.addresses[i] + " |");
         }
 
-        if (leaf == false) {
+        if (!isLeaf) {
             child[i].traverse();
         }
     }
@@ -35,15 +89,15 @@ public class Node {
     // Function to search for key in subtree rooted with this node
     public Node search(int key){
         int i = 0;
-        while (i < this.keycount && key > this.key[i*2]) {
+        while (i < this.keycount && key > this.keys[i]) {
             i++;
         }
 
-        if (this.key[i*2] == key) {
+        if (this.keys[i] == key && i < this.keycount) {
             return this;
         }
 
-        if (this.leaf == true) {
+        if (this.isLeaf) {
             return null;
         }
 
