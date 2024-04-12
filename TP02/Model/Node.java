b0004229e067c59111c8f88c0db8d70a441d774e
Rafael <rafaelolpeixoto@gmail.com>
2024-04-12 20:42:31 -0300

    BTree deletion

--- TP02/Model/Node.java
+++ TP02/Model/Node.java
@@ -7,10 +7,9 @@ public class Node {
     Node[] child; // An array of child pointers
     int keycount; // Current number of keys
     boolean isLeaf; // Is true when node is isLeaf. Otherwise false
- 
+
     // Constructor
-    Node(int degree, boolean isLeaf)
-    {
+    Node(int degree, boolean isLeaf) {
         this.degree = degree;
         this.keys = new int[degree * 2 - 1];
         this.addresses = new int[degree * 2 - 1];
@@ -19,10 +18,10 @@ public class Node {
         this.isLeaf = isLeaf;
     }
 
-    void insertNotFull(int key, int address){
+    void insertNotFull(int key, int address) {
         int i = keycount - 1;
-        if (isLeaf){
-            while (i >= 0 && keys[i] > key){
+        if (isLeaf) {
+            while (i >= 0 && keys[i] > key) {
                 keys[i + 1] = keys[i];
                 addresses[i + 1] = addresses[i];
                 i--;
@@ -35,33 +34,33 @@ public class Node {
 
     // A utility function to split the child y of this node
     // Note that y must be full when this function is called
-    void splitChild(int i, Node fullNode){
+    void splitChild(int i, Node fullNode) {
         Node newNode = new Node(fullNode.degree, fullNode.isLeaf);
         newNode.keycount = degree - 1;
 
-        for (int j = 0; j < degree - 1; j++){ // Copy the last (degree - 1) keys of fullNode to newNode
+        for (int j = 0; j < degree - 1; j++) { // Copy the last (degree - 1) keys of fullNode to newNode
             newNode.keys[j] = fullNode.keys[j + degree];
             newNode.addresses[j] = fullNode.addresses[j + degree];
         }
 
-        if (!fullNode.isLeaf){ // If fullNode is not a leaf, then move all its child pointers and addresses
-            for (int j = 0; j < degree; j++){
+        if (!fullNode.isLeaf) { // If fullNode is not a leaf, then move all its child pointers and addresses
+            for (int j = 0; j < degree; j++) {
                 newNode.child[j] = fullNode.child[j + degree];
                 newNode.addresses[j] = fullNode.addresses[j + degree];
             }
         }
 
-        fullNode.keycount = degree - 1; 
+        fullNode.keycount = degree - 1;
 
-        for (int j = keycount; j >= i + 1; j--){// Move all child pointers one space ahead
+        for (int j = keycount; j >= i + 1; j--) {// Move all child pointers one space ahead
             child[j + 1] = child[j];
         }
 
         child[i + 1] = newNode;// Link the new child to this node
 
-        for (int j = keycount; j > i; j--){// Move all keys one space ahead
-            keys[j+1] = keys[j];
-            addresses[j+1] = addresses[j];
+        for (int j = keycount; j > i; j--) {// Move all keys one space ahead
+            keys[j + 1] = keys[j];
+            addresses[j + 1] = addresses[j];
         }
 
         keys[i] = fullNode.keys[degree - 1]; // Copy the middle key of fullNode to this node
@@ -71,11 +70,11 @@ public class Node {
     }
 
     // Function to traverse all nodes in a subtree rooted with this node
-    public void traverse(){
+    public void traverse() {
 
         int i = 0;
         for (i = 0; i < this.keycount; i++) {
-            if (!this.isLeaf ) {
+            if (!this.isLeaf) {
                 this.child[i].traverse();
             }
             System.out.print(" " + this.keys[i] + " " + this.addresses[i] + " |");
@@ -87,7 +86,7 @@ public class Node {
     }
 
     // Function to search for key in subtree rooted with this node
-    public Node search(int key){
+    public Node search(int key) {
         int i = 0;
         while (i < this.keycount && key > this.keys[i]) {
             i++;
@@ -102,6 +101,15 @@ public class Node {
         }
 
         return this.child[i].search(key);
+    }
+
+    public int find(int k) {
+        for (int i = 0; i < this.keycount; i++) {
+            if (this.keys[i] == k) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
